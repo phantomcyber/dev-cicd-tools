d6b8382d93dae8e4c80dc68e887571a59adb93a8
Peter <pzhou@splunk.com>
2022-02-25 09:19:05 -0800

    Adding custom semgrep rules to cover prodsec questions

---
+++ semgrep/rules/python/logging/soar_standard_logging.py
@@ -0,0 +1,18 @@
+import logging as logger
+
+from phantom import BaseConnector as Base
+
+
+class Connector(Base):
+    def __init__(self):
+        # ruleid: soar-standard-logging
+        print('foobar')
+        # ruleid: soar-standard-logging
+        logger.info('foobar')
+        # ruleid: soar-standard-logging
+        logger.getLogger().info('foobar')
+
+        # ok: soar-standard-logging
+        self.debug_print('foobar')
+        # ok: soar-standard-logging
+        self.save_progress('foobar')
