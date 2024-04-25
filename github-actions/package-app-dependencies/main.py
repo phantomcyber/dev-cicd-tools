113edf8d65b1bfc2725bf9eaa68ecc53b744d644
Scott Odle <sodle@splunk.com>
2024-04-25 09:31:12 -0600

    env

--- github-actions/package-app-dependencies/main.py
+++ github-actions/package-app-dependencies/main.py
@@ -2,6 +2,5 @@ import sys
 import os
 
 print(f'hello! {sys.executable}')
-print(f'HOST={os.environ.get("HOST_PYTHON_VERSION")}')
-print(f'TARGETS={os.environ.get("TARGET_PYTHON_VERSIONS")}')
+print(os.environ)
 print(os.getcwd())
