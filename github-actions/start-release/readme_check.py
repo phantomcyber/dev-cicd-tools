9dad22b810a2c34d2dc68aa099028a90f5efd6b3
kczernik <kczernik@splunk.com>
2024-11-04 12:00:17 +0100

    PAPP-34959 | readme check test

--- github-actions/start-release/readme_check.py
+++ github-actions/start-release/readme_check.py
@@ -3,18 +3,14 @@
 import sys
 import logging
 from venv import logger
-# sys.path.insert(1, "github-actions/start-release")
-# import build_docs.py
-
-
-def check_readme():
-    pass
+from build_docs import build_docs_from_html
+from pathlib import Path
 
 def main():
     logger.info("readme-check runs")
+    print("GENERATING README PRINT")
+    build_docs_from_html(connector_path=Path.cwd(), app_version=None)
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
-    logging.info("GENERATING README")
-    print("GENERATING README PRINT")
     sys.exit(main())
