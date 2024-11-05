65961f9ef5ab86c7a73ce1091d094f7ea4e15e80
kczernik <kczernik@splunk.com>
2024-11-05 13:25:39 +0100

    PAPP-34959 | trying out build_docks

--- github-actions/start-release/readme_check.py
+++ github-actions/start-release/readme_check.py
@@ -3,12 +3,12 @@
 import sys
 import logging
 from venv import logger
-from build_docs import build_docs_from_html
+from build_docs import build_docs
 from pathlib import Path
 
 def main():
     logger.info("readme-check runs")
-    build_docs_from_html(connector_path=Path.cwd(), app_version=None)
+    build_docs(connector_path=Path.cwd(), app_version=None)
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
