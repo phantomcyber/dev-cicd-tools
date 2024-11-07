06c096119332d48602c4137982a2f65ee8f4cfa8
kczernik <kczernik@splunk.com>
2024-11-07 09:26:56 +0100

    PAPP-34959 | arguments handling changed

--- github-actions/start-release/readme_check.py
+++ github-actions/start-release/readme_check.py
@@ -2,18 +2,17 @@
 import argparse
 import sys
 import logging
-from venv import logger
 from build_docs import main
 from pathlib import Path
 
 # def main():
 #     logger.info("readme-check runs")
 #     build_docs(connector_path=Path.cwd(), app_version=None)
-#
+
 def pass_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("connector_path", help="Path to the connector", default=Path.cwd())
-    parser.add_argument("from_html", nargs='?', default=False, help="Build from html instead of md")
+    parser.add_argument("connector_path", default=Path.cwd())
+    parser.add_argument("from_html", default=False)
     return parser.parse_args()
 
 if __name__ == '__main__':
