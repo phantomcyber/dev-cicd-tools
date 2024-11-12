dccd3d56516cbd293942a125e8a6c77c524a4a94
kczernik <kczernik@splunk.com>
2024-11-12 12:50:21 +0100

    PAPP-34959 | code cleaned up

--- github-actions/start-release/readme_check.py
+++ github-actions/start-release/readme_check.py
@@ -12,19 +12,10 @@ def validate_dir_path(path):
     return path
 
 def pass_args():
-    default_path = Path().cwd()
-    logging.info(" PATH CWD DEFAULT %s", default_path)
     parser = argparse.ArgumentParser(allow_abbrev=False)
-    parser.add_argument("--connector_path", default=default_path, type=validate_dir_path)
+    parser.add_argument("--connector_path", default=Path.cwd(), type=validate_dir_path)
     parser.add_argument("--from_html", default=False, type=bool)
     args = parser.parse_args()
-    logging.info(" ARGS %s", args)
-    logging.info(" ARGS CONNECTOR PATH %s", args.connector_path)
-    logging.info(" ARGS FROM HTML %s", args.from_html)
-    logging.info(" PATH CWD %s", Path.cwd())
-    logging.info(" PATH CWD DEFAULT %s", default_path)
-    logging.info(" PATH FROM ARGS %s", args.connector_path)
-    logging.info(" ARGS %s", sys.argv)
     return args
 
 if __name__ == '__main__':
