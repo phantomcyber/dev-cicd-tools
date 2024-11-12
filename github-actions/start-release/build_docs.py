fd6dc88322b80a0d64d728eba4a252bd19ff2dcc
kczernik <kczernik@splunk.com>
2024-11-12 13:59:58 +0100

    PAPP-34959 | handling lack of .json file

--- github-actions/start-release/build_docs.py
+++ github-actions/start-release/build_docs.py
@@ -72,9 +72,14 @@ def escape_markdown(data):
 
 def get_app_json(app_json_dir):
     logging.info("Looking for app JSON in: %s", app_json_dir)
-    app_json_name = [f for f in os.listdir(app_json_dir)
-                     if f.endswith(".json")
-                     and "postman_collection" not in f][0]
+    app_json_name = ""
+    try:
+        app_json_name = [f for f in os.listdir(app_json_dir)
+                         if f.endswith(".json")
+                         and "postman_collection" not in f][0]
+    except IndexError:
+        logging.error("Unable to find app JSON in $s", app_json_dir)
+        sys.exit(1)
     json_file_path = Path(app_json_dir, app_json_name)
     logging.info("Loading json: %s", app_json_name)
     with open(json_file_path, "r") as json_file:
