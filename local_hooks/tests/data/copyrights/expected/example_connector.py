530e9cadc0ca217a9561f44ae93b464d7cf2aa40
subrata-hcltech <skamila@splunk.com>
2026-02-11 12:55:40 +0530

    onboard new app - apivoid v2 (#208)
    * onboard new app - apivoid v2
    
    ---------
    
    Co-authored-by: skmaila <skmaila@splunk.com>

--- local_hooks/tests/data/copyrights/expected/example_connector.py
+++ local_hooks/tests/data/copyrights/expected/example_connector.py
@@ -1,6 +1,6 @@
 # File: example_connector.py
 #
-# Copyright (c) 2017-2025 Splunk Inc.
+# Copyright (c) 2017-2026 Splunk Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
