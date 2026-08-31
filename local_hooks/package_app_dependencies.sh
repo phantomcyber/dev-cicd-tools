26b8076fb5729ead000f219eae746c613b581085
Athreya K M <athreyam@splunk.com>
2026-08-31 11:05:39 +0530

    chore: move local hooks tooling to Python 3.13 (#258)
    * chore: move local hooks tooling to python 3.13
    
    * ci: update semgrep test dependency
    
    * fix: upgrade django to 5.2

--- local_hooks/package_app_dependencies.sh
+++ local_hooks/package_app_dependencies.sh
@@ -6,7 +6,7 @@ PY39_BIN="/opt/python/cp39-cp39/bin"
 PY313_BIN="/opt/python/cp313-cp313/bin"
 
 # If we're not in a docker container, push ourselves into one and execute the script there
-if ! "${PY39_BIN}/python" --version &>/dev/null; then
+if ! "${PY313_BIN}/python" --version &>/dev/null; then
 	if ! docker info &>/dev/null; then
 		echo 'Please ensure Docker is installed and running on your machine'
 		exit 1
@@ -20,26 +20,25 @@ if ! "${PY39_BIN}/python" --version &>/dev/null; then
 	exec docker run \
 		--rm \
 		-v "$APP_DIR":/src \
-		-v "$(dirname "$0")":/srv/ \
+		-v "$(dirname "$0")":/srv/local_hooks \
 		-v "$PY_SITE":/site-packages \
-		-e "PYTHONPATH=/site-packages" \
+		-e "PYTHONPATH=/srv:/site-packages" \
 		-w /src \
 		"$IMAGE" \
-		/bin/bash -c "/srv/$script_name"
+		/bin/bash -c "/srv/local_hooks/$script_name"
 fi
 
 export PATH="$PY39_BIN:$PY313_BIN:$PATH"
 
 HOOKS_PYTHONPATH="${PYTHONPATH:-}"
+PACKAGER_PYTHON="${PY313_BIN}/python"
 
-# Sanity check: We can import local_hooks, right? If not, it's probably because we were already
-# running in a docker container and we didn't volume mount the site-packages directory
-# If that's the case, try to import local_hooks from pre-commit Python's site-packages
-if ! python -c 'import local_hooks'; then
-	#shellcheck disable=SC2034,SC2046
-	HOOKS_PYTHONPATH="$($(dirname "$0")/python -c 'import site; print(site.getsitepackages()[0])')"
+# Sanity check: We can import local_hooks, right? If not, try the source mount path used by
+# the Docker wrapper.
+if ! "$PACKAGER_PYTHON" -c 'import local_hooks'; then
+	HOOKS_PYTHONPATH="/srv:${HOOKS_PYTHONPATH}"
 	export PYTHONPATH="$HOOKS_PYTHONPATH"
-	python -c 'import local_hooks'
+	"$PACKAGER_PYTHON" -c 'import local_hooks'
 fi
 
 # The mounted pre-commit site-packages may contain host pip code from a newer Python version.
@@ -48,14 +47,14 @@ unset PYTHONPATH
 
 py39_deps='pip39_dependencies'
 py313_deps='pip313_dependencies'
-SCRIPT="python -m local_hooks.package_app_dependencies"
+SCRIPT=("$PACKAGER_PYTHON" -m local_hooks.package_app_dependencies)
 
 # Restore the hook import path only for the packager itself; child pip processes clear it again.
 run_packager() {
 	if [[ -n "${HOOKS_PYTHONPATH:-}" ]]; then
-		PYTHONPATH="$HOOKS_PYTHONPATH" ${SCRIPT} "$@"
+		PYTHONPATH="$HOOKS_PYTHONPATH" "${SCRIPT[@]}" "$@"
 	else
-		${SCRIPT} "$@"
+		"${SCRIPT[@]}" "$@"
 	fi
 }
 
@@ -65,9 +64,9 @@ has_pip_dependency_key() {
 	check_dependency_key='from local_hooks.package_app_dependencies import _should_package_pip_dependency_key; import sys; sys.exit(not _should_package_pip_dependency_key(".", sys.argv[1]))'
 
 	if [[ -n "${HOOKS_PYTHONPATH:-}" ]]; then
-		PYTHONPATH="$HOOKS_PYTHONPATH" python -c "$check_dependency_key" "$dependency_key"
+		PYTHONPATH="$HOOKS_PYTHONPATH" "$PACKAGER_PYTHON" -c "$check_dependency_key" "$dependency_key"
 	else
-		python -c "$check_dependency_key" "$dependency_key"
+		"$PACKAGER_PYTHON" -c "$check_dependency_key" "$dependency_key"
 	fi
 }
 
